8f8319e8a6df264ee969f82850d6f64bed878b6a
johan <johan.coppieters@howest.be>
2014-09-11 09:40:46 +0200

    added index.js to cody web

---
+++ codyweb/index.js
@@ -0,0 +1,47 @@
+//
+// Johan Coppieters - jan 2013 - Cody CMS
+//
+// website
+//
+//
+
+var express = require('express');
+var cody = require('cody');
+
+cody.server = express();
+
+
+// startup a routing for all static content of cody (images, javascript, css)
+cody.server.get("/cody/static/*", function (req, res) {
+    var fileserver = new cody.Static(req, res, "");
+    fileserver.serve();
+});
+
+
+cody.startWebApp(cody.server, {
+    "name": "codyweb",
+    "mailFrom": "info@cody-cms.org",
+    "smtp": "smtpmailer.howest.be",
+    "version": "V0.1",
+    "defaultlanguage": "en",
+    "hostnames" : "cody.local,vpn.cody-cms.org,howest.cody-cms.org,www.cody-cms.org,cody-cms.org",
+    "dbuser": "cody",
+    "dbpassword": "ydoc",
+    "dbhost": "localhost",
+    "datapath": "/usr/local/data/codyweb",
+    "db": "codyweb",
+    "controllers": require("./controllers/")
+  },
+  function() {
+    console.log("Loaded all apps....");
+    cody.server.listen(3001);
+    console.log('Listening on port ' + cody.server.get('port'));
+  }
+);
+
+
+if (!process.stderr.isTTY) {
+    process.on('uncaughtException', function (err) {
+        console.error('Uncaught exception : ' + err.stack);
+    });
+}
